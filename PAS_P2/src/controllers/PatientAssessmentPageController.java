02c2ed1f00c581c98b8431bc07b4bbabc798e6f0
KevinH72 <kevinhackett72@gmail.com>
2015-04-17 16:30:25 +0100

    commit test

--- PAS_P2/src/controllers/PatientAssessmentPageController.java
+++ PAS_P2/src/controllers/PatientAssessmentPageController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import app.Patient;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class PatientAssessmentPageController implements Initializable {
@@ -11,4 +15,32 @@ public class PatientAssessmentPageController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
+	
+	@FXML
+	private void btnConfirm(ActionEvent event) throws IOException {
+	}
+	
+	@FXML
+	private void btnCancel(ActionEvent event) throws IOException {
+	}
+	
+	
+	/**
+	 * sets relevant labels to patient information from the database these
+	 * labels are set before screen is changed --- see btnPatientInfo on the
+	 * ReceptionistHomePageController for more info ---
+	 * 
+	 * @param patient
+	 */
+	public void setPatientInfo(Patient patient) {
+//		nhsNumberLabel.setText(patient.getNhsNumber());
+//		titleLabel.setText(patient.getTitle());
+//		firstNameLabel.setText(patient.getFirstName());
+//		lastNameLabel.setText(patient.getLastName());
+//		streetNumberLabel.setText(patient.getStreetNumber());
+//		streetNameLabel.setText(patient.getStreetName());
+//		cityLabel.setText(patient.getCity());
+//		postCodeLabel.setText(patient.getPostCode());
+	}
+	
 }
--- PAS_P2/src/controllers/TriageNurseHomePageController.java
+++ PAS_P2/src/controllers/TriageNurseHomePageController.java
@@ -2,18 +2,61 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import app.Patient;
+import app.Queue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class TriageNurseHomePageController implements Initializable {
+
+	@FXML
+	private TableView<Patient> tableView;
+	
+	@FXML
+	private TableColumn<Patient, String> firstNameColumn;
+
+	@FXML
+	private TableColumn<Patient, String> lastNameColumn;
+	
+	private ObservableList<Patient> tableData;
+	public static List<Patient> displayQueue;
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		assert tableView != null : "fx:id=\"tableView\" was not injected: check your FXML file 'FXMLTriageNurseHomePage.fxml'";
+
+		// firstNameColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("firstName"));
+		// lastNameColumn.setCellValueFactory(new PropertyValueFactory<Patient, String>("lastName"));
+		// timeEnteredColumn.setCellValueFactory(new PropertyValueFactory<Patient, Date>("timeStamp"));
+	}
+	
+	/**
+	 * button for triage nurse to assess patient
+	 * @param event
+	 * @throws IOException
+	 */
 	@FXML
 	private void btnAssess(ActionEvent event) throws IOException {
+		
+		System.out.println("Changing to assess patient for triage screen");
+		
+		Patient patient = new Patient();
+		
 		System.out.println("Changing to patient assessment screen");
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/FXMLPatientAssessmentPage.fxml"));
 		loader.setLocation(getClass().getResource("/views/FXMLPatientAssessmentPage.fxml"));
@@ -21,11 +64,44 @@ public class TriageNurseHomePageController implements Initializable {
 		Parent p = loader.getRoot();
 		Stage stage = new Stage();
 		stage.setScene(new Scene(p));
-	}
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
+		// instance of the patient assessment page controller is created
+		// set it equal to the FXMLLoader controller that was just loaded
+		PatientAssessmentPageController patientAssessmentPageController = loader.<PatientAssessmentPageController>getController();
+		patientAssessmentPageController.setPatientInfo(patient);
+
+		stage.show();
+		
+	}
+	
+	@FXML
+	private void btnRefreshQueueClick(ActionEvent event) throws IOException{
+		displayQueue = Queue.queue;
+		displayQueue(displayQueue);
+	}
+
+	public void displayQueue(List<Patient> queue) {
+		tableData = FXCollections.observableArrayList(queue);
+		tableView.setItems(tableData);
+	}
+	
+	
+	/**
+	 * button to send user back to login screen
+	 * @param event
+	 * @throws Exception
+	 */
+	@FXML
+	private void btnLogout(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/views/FXMLLoginPage.FXML"));
+		loader.load();
+		Parent p = loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.show();
+		// hides current page
+		((Node) (event.getSource())).getScene().getWindow().hide();
 	}
 
 }
