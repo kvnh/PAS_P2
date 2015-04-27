db50e71a498c2a1b0536c89452bd435197231b63
Liam Connolly <lconnolly17@qub.ac.uk>
2015-04-27 10:27:53 +0100

    Logout button sorted on Doctor Assessment page

--- PAS_P2/src/controllers/DoctorAssessmentPageController.java
+++ PAS_P2/src/controllers/DoctorAssessmentPageController.java
@@ -3,7 +3,14 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class DoctorAssessmentPageController implements Initializable {
 
@@ -11,6 +18,20 @@ public class DoctorAssessmentPageController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@FXML
+	private void btnLogout(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/views/FXMLLoginPage.FXML"));
+		loader.load();
+		Parent p = loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.show();
+		stage.centerOnScreen();
+		// hides current page
+		((Node) (event.getSource())).getScene().getWindow().hide();
 	}
 
 }
