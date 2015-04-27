3c55d4267fe2d20c59393ec3968acf58036f7bff
Ciaran Gollogly <c_gollogly@msn.com>
2015-04-27 14:46:42 +0100

    Created controlQueue class which will control the thread in the queue class when instantiated.

---
+++ PAS_P2/src/app/ControlQueue.java
@@ -0,0 +1,37 @@
+package app;
+
+import java.util.Iterator;
+
+import controllers.QueueTabPageController;
+import objects.Patient;
+
+public class ControlQueue implements Runnable {
+
+	@Override
+	public void run() {
+		Iterator<Patient> it = Queue.queue.iterator();
+
+		while (it.hasNext()) {
+			
+			// sort queue
+			Queue.sortQueue();
+			
+			// refresh button method should be added here
+			
+
+			// about to take off element
+			if (Queue.queue.isEmpty()) {
+				System.out.println("List Empty");
+			} else {
+				Queue.addToTreatmentRoon();
+
+			}
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				System.out.println("Problems");
+			}
+		}
+	}
+
+}
