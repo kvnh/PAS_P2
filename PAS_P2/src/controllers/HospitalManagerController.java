51d1058f2da30028d1782bf764fc622c5f97f054
davidajcullen <davidajcullen@gmail.com>
2015-04-29 14:38:38 +0100

    fixed hospital manager page

--- PAS_P2/src/controllers/HospitalManagerController.java
+++ PAS_P2/src/controllers/HospitalManagerController.java
@@ -3,7 +3,14 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HospitalManagerController implements Initializable {
 
@@ -12,5 +19,22 @@ public class HospitalManagerController implements Initializable {
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * button to send user back to login screen
+	 * @param event
+	 * @throws Exception
+	 */
+	@FXML
+	private void btnLogout(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/views/FXMLLoginPage.FXML"));
+		loader.load();
+		Parent p = loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.show();
+		stage.centerOnScreen();
+		// hides current page
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
 }
