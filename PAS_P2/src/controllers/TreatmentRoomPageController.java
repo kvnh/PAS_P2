e2a544cbf43b2375a3347594239ec18d0c05bfec
davidajcullen <davidajcullen@gmail.com>
2015-04-29 10:14:39 +0100

    Added overall style and layout consistency to the whole application.

--- PAS_P2/src/controllers/TreatmentRoomPageController.java
+++ PAS_P2/src/controllers/TreatmentRoomPageController.java
@@ -3,14 +3,39 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TreatmentRoomPageController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
-
+	/**
+	 * button to send user back to login screen
+	 * @param event
+	 * @throws Exception
+	 */
+	@FXML
+	private void btnLogout(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/views/FXMLLoginPage.FXML"));
+		loader.load();
+		Parent p = loader.getRoot();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(p));
+		stage.show();
+		stage.centerOnScreen();
+		// hides current page
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
 }
